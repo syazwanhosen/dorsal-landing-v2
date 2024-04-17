c4f7adc5e36871c605f51db5af7ce0abdbb92e09
abrarfrahman <abrarfrahman@gmail.com>
2024-04-17 09:11:19 -0700

    adjust tsconfig

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { ThemeProvider } from "@/components/theme-provider.tsx";
+import App from "./App";
+import { ThemeProvider } from "@/components/theme-provider";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
