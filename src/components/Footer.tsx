4501dd8e6e7f740e3166b62e977d5c5279c965e7
abrarfrahman <abrarfrahman@gmail.com>
2024-04-16 18:42:55 -0700

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,126 @@
+import { LogoIcon } from "./Icons";
+
+export const Footer = () => {
+  return (
+    <footer id="footer">
+      <hr className="w-11/12 mx-auto" />
+
+      <section className="container py-20 grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
+        <div className="col-span-full xl:col-span-2">
+          <a
+            href="/"
+            className="font-bold text-xl flex"
+          >
+            <LogoIcon />
+            dorsal.fyi
+          </a>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h3 className="font-bold text-lg">Follow Us</h3>
+          <div>
+            <a
+              href="#"
+              className="opacity-60 hover:opacity-100"
+            >
+              Github
+            </a>
+          </div>
+
+          <div>
+            <a
+              href="#"
+              className="opacity-60 hover:opacity-100"
+            >
+              Twitter
+            </a>
+          </div>
+
+          <div>
+            <a
+              href="#"
+              className="opacity-60 hover:opacity-100"
+            >
+              Substack
+            </a>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h3 className="font-bold text-lg">About</h3>
+          <div>
+            <a
+              href="#"
+              className="opacity-60 hover:opacity-100"
+            >
+              Features
+            </a>
+          </div>
+
+          <div>
+            <a
+              href="#"
+              className="opacity-60 hover:opacity-100"
+            >
+              Pricing
+            </a>
+          </div>
+
+          <div>
+            <a
+              href="#"
+              className="opacity-60 hover:opacity-100"
+            >
+              FAQ
+            </a>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h3 className="font-bold text-lg">Coming Soon: Community</h3>
+          <div>
+            <a
+              href="#"
+              className="opacity-60 hover:opacity-100"
+            >
+              Discord
+            </a>
+          </div>
+
+          <div>
+            <a
+              href="#"
+              className="opacity-60 hover:opacity-100"
+            >
+              YouTube 
+            </a>
+          </div>
+
+          <div>
+            <a
+              href="#"
+              className="opacity-60 hover:opacity-100"
+            >
+              Facebook
+            </a>
+          </div>
+
+          
+        </div>
+      </section>
+
+      <section className="container pb-14 text-center">
+        <h3>
+          &copy; Dorsal.fyi 2024. Made with ♡ in CA. {" "}
+          <a
+            target="_blank"
+            href="https://github.com/abrarfrahman"
+            className="text-primary transition-all border-primary hover:border-b-2"
+          >
+            Abrar Rahman
+          </a>
+        </h3>
+      </section>
+    </footer>
+  );
+};
---
+++ src/components/Team.tsx
@@ -0,0 +1,130 @@
+import { buttonVariants } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Facebook, Instagram, Linkedin } from "lucide-react";
+
+interface TeamProps {
+  imageUrl: string;
+  name: string;
+  position: string;
+  socialNetworks: SociaNetworkslProps[];
+  bio: string;
+}
+
+interface SociaNetworkslProps {
+  name: string;
+  url: string;
+}
+
+const teamList: TeamProps[] = [
+  {
+    imageUrl: "./src/assets/abrar-rahman.jpg",
+    name: "Abrar Rahman",
+    position: "CEO",
+    socialNetworks: [
+      { name: "Linkedin", url: "https://www.linkedin.com/in/abrarfrahman/" },
+      {
+        name: "Instagram",
+        url: "https://www.instagram.com/abrarramen/",
+      },
+    ],
+    bio: "Prev. at Epic Systems, Amazon, Tesla, Berkeley Lab (LBNL)"
+  },
+  {
+    imageUrl: "./src/assets/andy-young.jpeg",
+    name: "Andy Young",
+    position: "COO",
+    socialNetworks: [
+      { name: "Linkedin", url: "https://www.linkedin.com/in/andyyru/" },
+      {
+        name: "Instagram",
+        url: "https://www.instagram.com/andyyru/",
+      },
+    ],
+    bio: "Prev. 4x Investment Banking Analyst, 1x Private Equity Analyst"
+  }
+];
+
+export const Team = () => {
+  const socialIcon = (iconName: string) => {
+    switch (iconName) {
+      case "Linkedin":
+        return <Linkedin size="20" />;
+
+      case "Facebook":
+        return <Facebook size="20" />;
+
+      case "Instagram":
+        return <Instagram size="20" />;
+    }
+  };
+
+  return (
+    <section
+      id="team"
+      className="container py-24 sm:py-32"
+    >
+      <h2 className="text-3xl md:text-4xl font-bold">
+        Our {" "}
+        <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+          Team
+        </span>
+      </h2>
+
+      <p className="mt-4 mb-10 text-xl text-muted-foreground">
+        Just two friends from Berkeley working to save Americans billions.
+      </p>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8 gap-y-10">
+        {teamList.map(
+          ({ imageUrl, name, position, socialNetworks, bio }: TeamProps) => (
+            <Card
+              key={name}
+              className="bg-muted/50 relative mt-8 flex flex-col justify-center items-center"
+            >
+              <CardHeader className="mt-8 flex justify-center items-center pb-2">
+                <img
+                  src={imageUrl}
+                  alt={`${name} ${position}`}
+                  className="absolute -top-12 rounded-full w-24 h-24 aspect-square object-cover"
+                />
+                <CardTitle className="text-center">{name}</CardTitle>
+                <CardDescription className="text-primary">
+                  {position}
+                </CardDescription>
+              </CardHeader>
+
+              <CardContent className="text-center pb-2">
+                <p>{bio}</p>
+              </CardContent>
+
+              <CardFooter>
+                {socialNetworks.map(({ name, url }: SociaNetworkslProps) => (
+                  <div key={name}>
+                    <a
+                      href={url}
+                      target="_blank"
+                      className={buttonVariants({
+                        variant: "ghost",
+                        size: "sm",
+                      })}
+                    >
+                      <span className="sr-only">{name} icon</span>
+                      {socialIcon(name)}
+                    </a>
+                  </div>
+                ))}
+              </CardFooter>
+            </Card>
+          )
+        )}
+      </div>
+    </section>
+  );
+};
