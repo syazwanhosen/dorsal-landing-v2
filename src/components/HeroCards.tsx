fbde2dc57bf3b9104bba35a7868b13634af91f27
abrarfrahman <abrarfrahman@gmail.com>
2024-04-17 21:23:19 -0700

    commenting out compliance mockups

--- src/components/HeroCards.tsx
+++ src/components/HeroCards.tsx
@@ -1,16 +1,9 @@
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { LockIcon, ShieldIcon } from "./Icons";
-
 export const HeroCards = () => {
   return (
     <div className="hidden lg:flex flex-row flex-wrap justify-center items-center gap-8 relative w-[700px] h-[500px]">
-    {/* HIPAA Compliance Card */}
-    <Card className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[350px] drop-shadow-xl shadow-black/10 dark:shadow-white/10 mb-5">
+      {/* HIPAA Compliance Card */}
+      {/*
+      <Card className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[350px] drop-shadow-xl shadow-black/10 dark:shadow-white/10 mb-5">
         <CardHeader className="space-y-1 flex md:flex-row justify-start items-start gap-4">
             <div className="mt-1 bg-primary/20 p-1 rounded-2xl">
                 <LockIcon />
@@ -22,10 +15,12 @@ export const HeroCards = () => {
                 </CardDescription>
             </div>
         </CardHeader>
-    </Card>
+      </Card>
+      */}
 
-    {/* SOC2 Compliance Card */}
-    <Card className="absolute top-[300px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[350px] drop-shadow-xl shadow-black/10 dark:shadow-white/10 mt-5">
+      {/* SOC2 Compliance Card */}
+      {/*
+      <Card className="absolute top-[300px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[350px] drop-shadow-xl shadow-black/10 dark:shadow-white/10 mt-5">
         <CardHeader className="space-y-1 flex md:flex-row justify-start items-start gap-4">
             <div className="mt-1 bg-primary/20 p-1 rounded-2xl">
                 <ShieldIcon />
@@ -37,8 +32,8 @@ export const HeroCards = () => {
                 </CardDescription>
             </div>
         </CardHeader>
-    </Card>
-</div>
-
+      </Card>
+      */}
+    </div>
   );
 };
