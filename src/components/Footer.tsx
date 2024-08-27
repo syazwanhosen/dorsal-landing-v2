b30b33adb6183127208c032392c022876ff6c895
abrarfrahman <abrarfrahman@gmail.com>
2024-08-26 21:50:38 -0700

    comment out most of footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,9 @@ import { LogoIcon } from "./Icons";
 export const Footer = () => {
   return (
     <footer id="footer">
+      {/*
       <hr className="w-11/12 mx-auto" />
-
+      
       <section className="container py-20 grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
         <div className="col-span-full xl:col-span-2">
           <a
@@ -126,17 +127,20 @@ export const Footer = () => {
           </div>
         </div>
       </section>
+      */}
 
       <section className="container pb-14 text-center">
+        <a
+          href="/"
+          className="font-bold text-xl flex"
+          style={{ fontFamily: 'the-seasons, sans-serif' }}
+        >
+          <LogoIcon /> 
+        </a>
+        
         <h3>
           &copy; Dorsal.fyi 2024. Made with ♡ in CA. {" "}
-          <a
-            target="_blank"
-            href="https://github.com/abrarfrahman"
-            className="text-primary transition-all border-primary hover:border-b-2"
-          >
-            Abrar Rahman
-          </a>
+
         </h3>
       </section>
     </footer>
