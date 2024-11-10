e494b1af8327bcb8a35b2085275a1d5792ab60c1
abrarfrahman <abrarfrahman@gmail.com>
2024-11-10 14:56:16 -0800

    remove unessesary import

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
-import { HowItWorks } from "./components/HowItWorks";
 import { Navbar } from "./components/Navbar";
 import { Newsletter } from "./components/Newsletter";
 import { Pricing } from "./components/Pricing";
 import { ScrollToTop } from "./components/ScrollToTop";
-import { Team } from "./components/Team";
 import { FAQ } from "./components/FAQ";
 
 import { Services } from "./components/Services";
