f03787ee759a293045cfb574336d290bb230ea38
syazwan <syazwanhosen@gmail.com>
2025-05-14 15:41:37 +0800

    fix: load leaflet css

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
 import AppRouter from "./AppRouter";
 import "./App.css";
+import "leaflet/dist/leaflet.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
