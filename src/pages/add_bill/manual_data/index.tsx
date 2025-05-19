fb09b29abf02c2d38f960106ca4f006acd304b71
syazwan <syazwanhosen@gmail.com>
2025-05-19 11:07:52 +0800

    chore: change heading tag

--- src/pages/add_bill/manual_data/index.tsx
+++ src/pages/add_bill/manual_data/index.tsx
@@ -4,10 +4,10 @@ import { UpdateDataForm } from "@/components/add_bill";
 export default function UploadDocumentPage() {
     return (
         <>
-            <h1 className="text-lg font-medium text-[#8770BC]">Enter Manual Data</h1>
+            <h2 className="text-lg font-medium text-[#8770BC]">Enter Manual Data</h2>
             <Card className="max-w-xl mx-auto mt-10 border-gray-300">
                 <CardContent className="p-6">
-                    <h1 className="text-lg font-medium mb-2">Enter Data</h1>
+                    <h3 className="text-lg font-medium mb-2">Enter Data</h3>
                     <hr className="mb-4 border-gray-200" />
                     <UpdateDataForm />
                 </CardContent>
