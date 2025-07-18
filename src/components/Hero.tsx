c44cd96540101fb7910c9153dcbdb84699d39e4f
syazwanhosen <syazwanhosen@gmail.com>
2025-07-18 15:19:56 +0800

    chore: update the search bar font size, remove search bar and replace with buttons

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import { Search, FileText } from "lucide-react";
+import { Hospital, FileText } from "lucide-react";
 
 export const Hero = () => {
   return (
@@ -14,47 +14,24 @@ export const Hero = () => {
         </span>
 
         {/* Title */}
-        <h1 className="text-4xl sm:text-5xl font-semibold text-gray-900 leading-tight mb-4">
+        <h1 className="text-7xl font-semibold text-gray-900 leading-tight mb-4">
           Fight <span className="text-pink">smarter</span>, not harder.
         </h1>
         <p className="text-gray-600 text-lg max-w-xl">
           Insights on pricing, procedures, providers and more!
         </p>
 
-        {/* Common Search Tags */}
-        <div className="flex flex-wrap justify-center gap-4 mt-8">
-          {["MRI with contrast", "Colonoscopy", "Cesarean"].map((item) => (
-            <button
-              key={item}
-              className="flex items-center gap-2 px-4 py-2 border border-[#8770BC] text-[#8770BC] rounded-full hover:shadow-md transition"
-            >
-              <span>{item}</span>
-              <Search size={16} />
-            </button>
-          ))}
-        </div>
-
-        {/* Search Input */}
-        <div className="mt-6 w-full max-w-xl flex items-center bg-white border border-gray-300 rounded-xl shadow-md2">
-          <Search size={16} className="mx-3"/>
-          <input
-            type="text"
-            placeholder="Search procedures, providers, or conditions..."
-            className="flex-grow outline-none text-gray-700 placeholder-gray-400 bg-transparent"
-          />
-          <button className="bg-purple hover:bg-purple-700 text-white rounded-xl px-6 py-2 text-sm transition">
-            Search
+        {/* CTA Button */}
+        <div className="flex gap-10">
+          <button className="mt-6 flex items-center gap-2 bg-purple hover:bg-purple-700 text-white font-semibold px-6 py-3 rounded-lg shadow-lg transition">
+            <Hospital size={18} />
+            SEARCH HOSPITAL
+          </button>
+          <button className="mt-6 flex items-center gap-2 bg-pink hover:bg-pink-700 text-white font-semibold px-6 py-3 rounded-lg shadow-lg transition">
+            <FileText size={18} />
+            NEGOTIATE YOUR BILL
           </button>
         </div>
-
-        {/* CTA Button */}
-        <button className="mt-6 flex items-center gap-2 bg-purple hover:bg-purple-700 text-white font-semibold px-6 py-3 rounded-lg shadow-lg transition">
-          <FileText size={18} />
-          NEGOTIATE YOUR BILL
-        </button>
-
-     
- 
       </div>
     </section>
   );
