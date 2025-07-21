4affe5bf9b4caaeb7cfe425cae600f88816aec11
abrarfrahman <abrarfrahman@gmail.com>
2025-07-20 17:54:57 -0700

    simplify coming soon pg

--- src/pages/coming-soon/ComingSoon.tsx
+++ src/pages/coming-soon/ComingSoon.tsx
@@ -1,7 +1,6 @@
 import Footer from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
 import { Newsletter } from "@/components/Newsletter";
-import womanImage from "@/assets/coming.webp";
 import { Calendar } from "lucide-react";
 
 const ComingSoon = () => {
@@ -10,32 +9,20 @@ const ComingSoon = () => {
       <Navbar />
       <div className="container text-center lg:pt-16 pt-4 pb-4 bg-white">
         <h1 className="md:text-7xl text-2xl font-bold">
-          Coming <span className="text-pink">Soon</span>
+          Coming <span className="text-pink">Soon!</span>
         </h1>
-        <h2 className="md:text-3xl md:my-6 my-3 text-gray-600">
-          Innovating the way you experience Dorsal
+        <h2 className="md:text-xl md:my-6 my-3 text-gray-600">
+          Fixing American healthcare, one bill at a time :)
         </h2>
         <a
           href="https://cal.com/abrar/15min"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-flex items-center md:text-lg text-sm gap-2 text-white bg-purple hover:bg-pink font-semibold px-4 py-3 rounded-lg transition-colors duration-200"
+          className="inline-flex items-center md:text-md text-sm gap-2 text-white bg-purple hover:bg-pink font-semibold px-4 py-3 rounded-lg transition-colors duration-200"
         >
           <Calendar className="w-5 h-5" />
-          BOOK A MEETING WITH US
+          Have a story or seeking a demo for your org?
         </a>
-
-        <div className="mt-8">
-          <div className="mt-8">
-            <img
-              src={womanImage}
-              alt="Illustration of a woman"
-              className="mx-auto w-full max-w-[800px] h-auto"
-              loading="lazy"
-              decoding="async"
-            />
-          </div>
-        </div>
       </div>
       <Newsletter />
       <Footer />
