7fd4a42ce32d482751a25e9c449500fde1c2164f
Bakhtawar Memon <bakhtawermemon193@gmail.com>
2025-07-25 13:52:08 +0800

    Updated the hospital search state condition and Top Hospital component static name Top hospitals - update list and link

--- src/components/hospitals/TopHospitals.tsx
+++ src/components/hospitals/TopHospitals.tsx
@@ -23,7 +23,7 @@ const topHospitals: TopSearchHospital[] = [
 
 export const TopHospitals: React.FC = () => {
   return (
-    <div className="border rounded-md p-4 w-full">
+    <div className="bg-[#F5F1FF] rounded-md py-4 px-4 lg:px-8 w-full">
       <div className="flex items-center mb-4">
         <div className="flex items-center justify-center w-10 h-10 lg:w-12 lg:h-12 lg:mr-4 mr-2 rounded-full bg-gradient-to-r from-[#9F70FD] to-[#E770C1]">
           <Hospital className="w-3 h-3 lg:w-5 lg:h-5 text-white" />
