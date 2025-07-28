231db77f190550a6e03041059068f02e1d79ac17
syazwanhosen <syazwanhosen@gmail.com>
2025-07-28 15:53:49 +0800

    chore: manually group vendor or large libs

--- vite.config.ts
+++ vite.config.ts
@@ -13,13 +13,41 @@ export default defineConfig({
     port: 4000,
     strictPort: true,
   },
-   build: {
+    build: {
     rollupOptions: {
       output: {
         manualChunks: {
-          'react-vendor': ['react', 'react-dom'],
-        },
-      },
-    },
-  },
+          react: ['react', 'react-dom', 'react-router-dom'],
+          redux: ['@reduxjs/toolkit', 'react-redux', 'redux-persist'],
+          charts: ['chart.js', 'react-chartjs-2', 'd3'],
+          maps: ['leaflet', 'react-leaflet', 'topojson-client'],
+          radix: [
+            '@radix-ui/react-accordion',
+            '@radix-ui/react-avatar',
+            '@radix-ui/react-checkbox',
+            '@radix-ui/react-collapsible',
+            '@radix-ui/react-dialog',
+            '@radix-ui/react-dropdown-menu',
+            '@radix-ui/react-icons',
+            '@radix-ui/react-label',
+            '@radix-ui/react-navigation-menu',
+            '@radix-ui/react-popover',
+            '@radix-ui/react-scroll-area',
+            '@radix-ui/react-slot',
+            '@radix-ui/react-tabs',
+            '@radix-ui/react-tooltip'
+          ],
+          ui: [
+            'clsx',
+            'class-variance-authority',
+            'lucide-react',
+            'sonner',
+            'react-icons',
+            'tailwind-merge',
+            'tailwindcss-animate'
+          ]
+        }
+      }
+    }
+  }
 });
