2127975e8b2ee88dd071a9ff71961007674892b7
syazwanhosen <syazwanhosen@gmail.com>
2025-08-01 20:17:32 +0800

    remove

--- src/pages/roadmap/feature-card.tsx
+++ src/pages/roadmap/feature-card.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import axios from "axios"
 
 import { Badge } from "@/components/ui/badge"
@@ -188,7 +188,7 @@ export function FeatureCard({ feature, featureMapping }: FeatureCardProps) {
   }
 
 
-  /(() => {
+  // useEffect(() => {
   //   if (!feature?.id) return;
 
   //   axios
